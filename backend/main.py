9ede97d60e9bd9f6c4febd310077aa2e3faa98e0
SreyVenk <157976631+SreyVenk@users.noreply.github.com>
2026-05-06 14:39:26 -0400

    Fix frontend submodule issue

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://pulsewatch-pq6v.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
